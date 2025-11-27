6273c309c70f0032b856d31ea4bc7223ab8dd62d
NadarianBerija <nadarian.beria@gmail.com>
2025-11-27 09:13:57 +0200

    viewAdmin/error404

---
+++ admin/viewAdmin/error404.php
@@ -0,0 +1,18 @@
+<?php
+ob_start()
+?>
+
+<h2>404 Error</h2>
+
+<article>
+    <h3>404 ошибка - что это такое?</h3>
+    <p>По запрашиваемому URL страница не найдена</p>
+</article>
+
+<?php
+$content = ob_get_clean();
+?>
+
+<?php
+include "viewAdmin/templates/layout.php";
+?>
