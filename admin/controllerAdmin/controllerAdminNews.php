dbc4f1a4ae86e85c312ce4dac24b5de08d96ffb8
NadarianBerija <nadarian.beria@gmail.com>
2025-11-27 12:27:02 +0200

    controllerAdminNews

---
+++ admin/controllerAdmin/controllerAdminNews.php
@@ -0,0 +1,8 @@
+<?php
+class controllerAdminNews{
+    public static function NewsList() {
+        $arr = modelAdminNews::getNewsList();
+        include_once 'viewAdmin/newsList.php';
+    }
+}
+?>
