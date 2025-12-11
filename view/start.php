d756c041e00740ff87a2d718b26ab2bce4efb385
NadarianBerija <nadarian.beria@gmail.com>
2025-12-11 15:17:04 +0200

    визуальные изменения

--- view/start.php
+++ view/start.php
@@ -3,8 +3,10 @@ ob_start();
 ?>
 <h1>TOP 3 NEWS</h1>
 <br>
+<div class="newsContainer">
 <?php
 ViewNews::NewsByCategory($arr);
 $content = ob_get_clean();
 include_once 'view/layout.php';
 ?>
+</div>
